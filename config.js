63442d00a9d2f36421ff6eaefd9b944855d46e05
ruhtroh <jhitchco@lion.lmu.edu>
2016-02-21 23:19:47 -0800

    First addition
    I've ben working on this code for a while. Originally started hacking from the previous creator @dariusk from github. Retweets posts from #mediaarts. I made it my own, starting to have the program search through twitter to find sales for certain shoes and then add them to cart instantly.

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'bTBXYzUPE4NHCy5qDHzcJ8o1D',
+  consumer_secret:      'Ke6M01PzF0ajnoOWrlsxBbxZ2n0x2XbTznSrjCPNQvqptpuMgK',
+  access_token:         '881011873-6ZVTk36cM7Wh1g9ihlb3BsM5Z8KftAK0xyC5MBXr',
+  access_token_secret:  '3kqTXnXeb15jzRQG1F61hGzW33E4tkUW6b74UxnRe6i11'
+}

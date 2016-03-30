0ab9f437eccfd0b8dfd7ab616f4aa2583e6ce770
Jeremy Hitchcock <jhitchco@lion.lmu.edu>
2016-03-29 23:51:16 -0700

    Resolved previous issue, made more idiomatic.
    instead of using for loops and lists, I simplified the code by using the base64 and binascii libraries.

--- convertToBase64.py
+++ convertToBase64.py
@@ -1,11 +1,8 @@
 import base64
+import binascii
 
 def splitByTwo(str):
     return [i+j for i,j in zip(list(str)[::2], list(str)[1::2])]
 
 def bytesToBase64(str):
-    b64List = []
-    stringsByTwo = splitByTwo(str.upper())
-    
-
-print(bytesToBase64("49276d206b696c639f3C4D8a"))
+    return base64.b64encode(binascii.unhexlify(str))

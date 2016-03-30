bcafd496e422825eb6ff5b923892c1d45c9b2b27
Jeremy Hitchcock <jhitchco@lion.lmu.edu>
2016-03-30 00:01:50 -0700

    Did not need the splitByTwo() method.

--- convertToBase64.py
+++ convertToBase64.py
@@ -1,8 +1,5 @@
 import base64
 import binascii
 
-def splitByTwo(str):
-    return [i+j for i,j in zip(list(str)[::2], list(str)[1::2])]
-
 def bytesToBase64(str):
     return base64.b64encode(binascii.unhexlify(str))

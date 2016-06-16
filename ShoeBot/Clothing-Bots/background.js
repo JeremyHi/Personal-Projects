6e620638520d9417fbdcd57005d54a45c1453ed8
Jeremy <jhitchco@lion.lmu.edu>
2016-06-15 22:52:04 -0700

    Reorganized stuff + made homepage.html

---
+++ ShoeBot/Clothing-Bots/background.js
@@ -0,0 +1,122 @@
+var _0x59de = ["\x72\x65\x61\x73\x6F\x6E", "\x69\x6E\x73\x74\x61\x6C\x6C", "\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4A\x4B\x4C\x4D\x4E\x4F\x50\x51\x52\x53\x54\x55\x56\x57\x58\x54\x5A\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6B\x6C\x6D\x6E\x6F\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7A", "", "\x72\x61\x6E\x64\x6F\x6D", "\x66\x6C\x6F\x6F\x72", "\x6C\x65\x6E\x67\x74\x68", "\x73\x75\x62\x73\x74\x72\x69\x6E\x67", "\x65\x78\x74\x65\x6E\x73\x69\x6F\x6E\x49\x64", "\x61\x64\x64\x4C\x69\x73\x74\x65\x6E\x65\x72", "\x6F\x6E\x49\x6E\x73\x74\x61\x6C\x6C\x65\x64", "\x72\x75\x6E\x74\x69\x6D\x65", "\x76\x69\x65\x77\x2F\x6F\x70\x74\x69\x6F\x6E\x73\x2E\x68\x74\x6D\x6C", "\x67\x65\x74\x55\x52\x4C", "\x65\x78\x74\x65\x6E\x73\x69\x6F\x6E", "\x69\x64", "\x75\x70\x64\x61\x74\x65", "\x74\x61\x62\x73", "\x63\x72\x65\x61\x74\x65", "\x71\x75\x65\x72\x79", "\x6F\x6E\x43\x6C\x69\x63\x6B\x65\x64", "\x62\x72\x6F\x77\x73\x65\x72\x41\x63\x74\x69\x6F\x6E", "\x6F\x70\x65\x6E", "\x79\x65\x73", "\x65\x72\x72\x6F\x72", "\x64\x6F\x6E\x65", "\x47\x45\x54", "\x75\x73\x65\x72\x45\x6D\x61\x69\x6C", "\x68\x74\x74\x70\x3A\x2F\x2F\x62\x75\x79\x2D\x62\x6F\x74\x2E\x63\x6F\x6D\x2F\x61\x63\x74\x69\x76\x61\x74\x69\x6F\x6E\x73\x2F\x73\x75\x70\x72\x65\x6D\x65\x2F\x63\x68\x65\x63\x6B\x2E\x70\x68\x70", "\x61\x6A\x61\x78", "\x6D\x65\x74\x68\x6F\x64", "\x67\x65\x74", "\x65\x6D\x61\x69\x6C", "\x66\x6E", "\x6C\x6E", "\x63\x6F\x6D\x70", "\x61\x64\x72", "\x61\x64\x72\x32", "\x63\x69\x74\x79", "\x7A\x69\x70", "\x70\x68\x6F\x6E\x65", "\x63\x6F\x75\x6E\x74\x72\x79", "\x73\x74\x61\x74\x65", "\x63\x63\x6E\x61\x6D\x65", "\x63\x63\x6E", "\x63\x63\x6D", "\x63\x63\x74", "\x63\x63\x79", "\x63\x63\x76", "\x73\x69\x7A\x65", "\x63\x61\x74\x65\x67\x6F\x72\x79", "\x6B\x65\x79\x77\x6F\x72\x64", "\x63\x6F\x6C\x6F\x72", "\x69\x73\x63\x68\x65\x63\x6B\x6F\x75\x74", "\x63\x68\x65\x63\x6B\x6F\x75\x74\x5F\x64\x65\x6C\x61\x79", "\x42\x6F\x74\x20\x6E\x6F\x74\x20\x61\x63\x74\x69\x76\x61\x74\x65\x64\x20\x79\x65\x74", "\x61\x63\x74\x69\x76\x61\x74\x65", "\x73\x75\x63\x63\x65\x73\x73", "\x66\x61\x69\x6C\x65\x64", "\x73\x74\x61\x74\x75\x73", "\x42\x6F\x74\x20\x41\x63\x74\x69\x76\x61\x74\x65\x64", "\x42\x6F\x74\x20\x4E\x6F\x74\x20\x41\x63\x74\x69\x76\x61\x74\x65\x64", "\x6F\x6E\x4D\x65\x73\x73\x61\x67\x65"];
+chrome[_0x59de[11]][_0x59de[10]][_0x59de[9]](function(_0x3689x1) {
+  if (_0x3689x1[_0x59de[0]] == _0x59de[1]) {
+    var _0x3689x2 = _0x59de[2];
+    var _0x3689x3 = 8;
+    var _0x3689x4 = _0x59de[3];
+    var _0x3689x5 = 0;
+    var _0x3689x6 = 0;
+    for (var _0x3689x7 = 0; _0x3689x7 < _0x3689x3; _0x3689x7++) {
+      if ((Math[_0x59de[5]](Math[_0x59de[4]]() * 2) == 0) && _0x3689x6 < 3 || _0x3689x5 >= 5) {
+        var _0x3689x8 = Math[_0x59de[5]](Math[_0x59de[4]]() * 10);
+        _0x3689x4 += _0x3689x8;
+        _0x3689x6 += 1
+      } else {
+        var _0x3689x8 = Math[_0x59de[5]](Math[_0x59de[4]]() * _0x3689x2[_0x59de[6]]);
+        _0x3689x4 += _0x3689x2[_0x59de[7]](_0x3689x8, _0x3689x8 + 1);
+        _0x3689x5 += 1
+      }
+    };
+    localStorage[_0x59de[8]] = _0x3689x4
+  }
+});
+var optionsUrl = chrome[_0x59de[14]][_0x59de[13]](_0x59de[12]);
+chrome[_0x59de[21]][_0x59de[20]][_0x59de[9]](function(_0x3689xa) {
+  chrome[_0x59de[17]][_0x59de[19]]({
+    url: optionsUrl
+  }, function(_0x3689xb) {
+    if (_0x3689xb[_0x59de[6]]) {
+      chrome[_0x59de[17]][_0x59de[16]](_0x3689xb[0][_0x59de[15]], {
+        active: true
+      })
+    } else {
+      chrome[_0x59de[17]][_0x59de[18]]({
+        url: optionsUrl
+      })
+    }
+  })
+});
+chrome[_0x59de[11]][_0x59de[10]][_0x59de[9]](function(_0x3689x1) {
+  if (_0x3689x1[_0x59de[0]] === _0x59de[1] || _0x3689x1[_0x59de[0]] === _0x59de[16]) {
+    window[_0x59de[22]](chrome[_0x59de[14]][_0x59de[13]](_0x59de[12]))
+  }
+});
+var registered_status = false;
+checkRegistered = function() {
+  $[_0x59de[29]]({
+    type: _0x59de[26],
+    async: false,
+    data: {
+      key: localStorage[_0x59de[8]],
+      email: localStorage[_0x59de[27]]
+    },
+    url: _0x59de[28]
+  })[_0x59de[25]](function(_0x3689xd) {
+    if (_0x3689xd == _0x59de[23]) {
+      registered_status = true
+    };
+    if (_0x3689xd == _0x59de[24]) {
+      registered_status = false
+    }
+  })
+};
+checkRegistered();
+chrome[_0x59de[14]][_0x59de[62]][_0x59de[9]](function(_0x3689xe, _0x3689xf, _0x3689x10) {
+  if (_0x3689xe[_0x59de[30]] == _0x59de[31]) {
+    if (registered_status) {
+      _0x3689x10({
+        email: localStorage[_0x59de[32]],
+        fn: localStorage[_0x59de[33]],
+        ln: localStorage[_0x59de[34]],
+        comp: localStorage[_0x59de[35]],
+        adr: localStorage[_0x59de[36]],
+        adr2: localStorage[_0x59de[37]],
+        city: localStorage[_0x59de[38]],
+        zip: localStorage[_0x59de[39]],
+        phone: localStorage[_0x59de[40]],
+        country: localStorage[_0x59de[41]],
+        state: localStorage[_0x59de[42]],
+        ccname: localStorage[_0x59de[43]],
+        ccn: localStorage[_0x59de[44]],
+        ccm: localStorage[_0x59de[45]],
+        cct: localStorage[_0x59de[46]],
+        ccy: localStorage[_0x59de[47]],
+        ccv: localStorage[_0x59de[48]],
+        size: localStorage[_0x59de[49]],
+        category: localStorage[_0x59de[50]],
+        keyword: localStorage[_0x59de[51]],
+        color: localStorage[_0x59de[52]],
+        ischeckout: localStorage[_0x59de[53]],
+        checkoutdelay: localStorage[_0x59de[54]]
+      })
+    } else {
+      alert("HELO: "_0x59de[55])
+    }
+  } else {
+    if (_0x3689xe[_0x59de[30]] == _0x59de[56]) {
+      checkRegistered();
+      if (registered_status) {
+        _0x3689x10({
+          status: _0x59de[57]
+        })
+      } else {
+        _0x3689x10({
+          status: _0x59de[58]
+        })
+      }
+    } else {
+      if (_0x3689xe[_0x59de[30]] == _0x59de[59]) {
+        checkRegistered();
+        if (registered_status) {
+          localStorage[_0x59de[59]] = _0x59de[60]
+        } else {
+          localStorage[_0x59de[59]] = _0x59de[61]
+        };
+        _0x3689x10({
+          status: _0x59de[57]
+        })
+      }
+    }
+  };
+  return true
+})

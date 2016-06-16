b66a1c72b0ec1ff5cea190b4f25e088755c8be2c
Jeremy Hitchcock <jhitchcock@Vinfolio-Jeremy.local>
2016-06-16 11:00:47 -0700

    Changed around some files

---
+++ Vincellar-Website/php/register.php
@@ -0,0 +1,9 @@
+<?php
+    $mysql_hostname = "localhost";
+    $mysql_username = "root";
+    $mysql_password = " Winston ";
+    $mysql_database = "VinCellar_Users";
+    $db = mysql_connect ($mysql_hostname, $mysql_username, $mysql_password) or die(mysql_error());
+
+    mysql_select_db($mysql_database, $db) or die("Couldn't find database");
+?>
--- Websites/Vincellar-Website/register.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $mysql_hostname = "localhost/~jhitchcock/";
-    $mysql_username = "root";
-    $mysql_password = " Winston ";
-    $mysql_database = "VinCellar_Users";
-    $db = mysql_connect ($mysql_hostname, $mysql_username, $mysql_password) or die("Something is broken :(");
-
-    mysql_select_db($mysql_database, $db) or die("Couldn't find database");
-?>

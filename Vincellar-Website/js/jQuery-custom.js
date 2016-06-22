4a6da95ebadfbd0952aca97e983bd6aaa7574803
Jeremy Hitchcock <jhitchcock@Vinfolio-Jeremy.local>
2016-06-22 12:28:11 -0700

    stylized navbar to fade in/fadeout

--- Vincellar-Website/js/jQuery-custom.js
+++ Vincellar-Website/js/jQuery-custom.js
@@ -1,8 +1,8 @@
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
           scrollTop: target.offset().top - 250
@@ -12,3 +12,26 @@ $(function() {
     }
   });
 });
+
+(function($) {
+  $(document).ready(function() {
+
+    // hide .navbar first
+    $("nav.navbar.navbar-default.navbar-fixed-top.blacked").hide();
+
+    // fade in nav.navbar.navbar-default.navbar-fixed-top.blacked
+    $(function() {
+      $(window).scroll(function() {
+        // set distance user needs to scroll before we fadeIn navbar
+        if ($(this).scrollTop() > 100) {
+          $('nav.navbar.navbar-default.navbar-fixed-top.blacked').fadeIn();
+        } else {
+          $('nav.navbar.navbar-default.navbar-fixed-top.blacked').fadeOut();
+        }
+      });
+
+
+    });
+
+  });
+}(jQuery));

275d55b8fb7051ee66041ec61fd7be4b4390f440
Jeremy <jhitchco@lion.lmu.edu>
2017-07-12 00:08:37 -0700

    Added way for 12h to grab previous prices via external list

--- Ethereum_Twitter/EthereumCheck.py
+++ Ethereum_Twitter/EthereumCheck.py
@@ -1,30 +1,38 @@
+#!/usr/bin/env python
+
 import json, requests, time
 import TwitterCredentials as tc
 from twython import Twython
 
-starttime = time.time()
-
-def hourly_update():
+def getTwitter():
   credentials = tc.getTwitterCredentials()
-
-  twitter = Twython(
+  return Twython(
     credentials['APP_KEY'],
-    credentials['APP_SECRECT'], 
-    credentials['OAUTH_TOKEN'], 
+    credentials['APP_SECRECT'],
+    credentials['OAUTH_TOKEN'],
     credentials['OAUTH_TOKEN_SECRET'])
 
+def getPrices():
   response_Coin_Makret_Cap = json.loads(requests.get("https://coinmarketcap-nexuist.rhcloud.com/api/eth").text)['price']['usd']
   response_Crypto_Compare = json.loads(requests.get("https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD").text)['USD']
   response_GDax = json.loads(requests.get("https://api.gdax.com/products/ETH-USD/ticker").text)['price']
-  
-  response_list = [float(i) for i in [response_Coin_Makret_Cap, response_Crypto_Compare, response_GDax]]
-  response_average = '{0:.2f}'.format(sum(response_list) / len(response_list))
+  return [float(i) for i in [response_Coin_Makret_Cap, response_Crypto_Compare, response_GDax]]
 
+def postToJSON(numbers):
+  with open('PriceData.txt', 'a') as outfile:
+    outfile.write(numbers + '\n')
+
+def hourly_update():
+  twitter = getTwitter()
+  response_list = getPrices()
+
+  response_average = '{0:.2f}'.format(sum(response_list) / len(response_list))
+  postToJSON(response_average)
   tweet = "The current price of Ethereum in USD is: $" + response_average + " #Ethereum #ETH"
 
   twitter.update_status(status=tweet)
   print(tweet)
-  
+
 try:
   hourly_update()
 except Exception as e:
--- Ethereum_Twitter/EthereumPrice12h.py
+++ Ethereum_Twitter/EthereumPrice12h.py
@@ -1,39 +1,42 @@
 #!/usr/bin/env python
 
-import json, requests, time
+import json, requests
 from twython import Twython
 import TwitterCredentials as tc
 
-starttime = time.time()
+def getPrices():
+  response_Coin_Makret_Cap = json.loads(requests.get("https://coinmarketcap-nexuist.rhcloud.com/api/eth").text)['price']['usd']
+  response_Crypto_Compare = json.loads(requests.get("https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD").text)['USD']
+  response_GDax = json.loads(requests.get("https://api.gdax.com/products/ETH-USD/ticker").text)['price']
+  return [float(i) for i in [response_Coin_Makret_Cap, response_Crypto_Compare, response_GDax]]
 
 def twelve_hour_update():
   credentials = tc.getTwitterCredentials()
-
   twitter = Twython(
     credentials['APP_KEY'],
-    credentials['APP_SECRECT'], 
-    credentials['OAUTH_TOKEN'], 
+    credentials['APP_SECRECT'],
+    credentials['OAUTH_TOKEN'],
     credentials['OAUTH_TOKEN_SECRET'])
 
-  response_Coin_Makret_Cap = json.loads(requests.get("https://coinmarketcap-nexuist.rhcloud.com/api/eth").text)['price']['usd']
-  response_Crypto_Compare = json.loads(requests.get("https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD").text)['USD']
-  current_price = round((response_Coin_Makret_Cap + response_Crypto_Compare) / 2, 2)
-
-  past_twelve_hours = twitter.get_user_timeline(screen_name="Current_ETH", count=12)
-
   twelve_hours_ago = 0
-  for tweet in past_twelve_hours:
-    if '$' in tweet['text']:
-      print(tweet['text'][tweet['text'].index('$')+1:tweet['text'].index('$')+7])
-      twelve_hours_ago+= float(tweet['text'][tweet['text'].index('$')+1:tweet['text'].index('$')+7])
+  counter = 1
+  for line in reversed(list(open("PriceData.txt"))):
+    if counter == 12: break
+    try:
+      print(float(line.rstrip()))
+      twelve_hours_ago += float(line.rstrip())
+    except Exception as e:
+      print(e)
+    counter += 1
 
-  percent_change = (current_price - (twelve_hours_ago/len(past_twelve_hours))) / twelve_hours_ago * 100
+  currentPrices = getPrices()
+  current_price = sum(currentPrices) / len(currentPrices)
 
+  percent_change = (current_price - (twelve_hours_ago/12)) / twelve_hours_ago * 100
   change = str(round(percent_change, 2)) + "% " + ("increase" if percent_change > 0 else "decrease")
 
   tweet = "In the past 12 hours, we have seen a " + change + " in the price of Ethereum to USD. #Ethereum #ETH"
-
-  twitter.update_status(status=tweet)
+  # twitter.update_status(status=tweet)
   print(tweet)
 
 try:

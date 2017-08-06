41523261092ba9660dc67b2628d1b6aa1757868e
Jeremy <jhitchco@lion.lmu.edu>
2017-08-06 12:10:33 -0700

    Twitter account now updates with news articles

--- Ethereum_Twitter/EthereumLinks.py
+++ Ethereum_Twitter/EthereumLinks.py
@@ -1,21 +1,38 @@
 #!/usr/bin/env python
 
-import json, requests, time
 import praw
 import RedditCredentials as rc
+import TwitterCredentials as tc
+from twython import Twython
 
 credentials = rc.getTwitterCredentials()
 reddit = praw.Reddit(
-	client_id=credentials['CLIENT_ID'],
-	client_secret=credentials['CLIENT_SECRECT'],
-	user_agent=credentials['USER_AGENT'])
+  client_id=credentials['CLIENT_ID'],
+  client_secret=credentials['CLIENT_SECRECT'],
+  user_agent=credentials['USER_AGENT'])
+
+def getTwitter():
+  credentials = tc.getTwitterCredentials()
+  return Twython(
+    credentials['APP_KEY'],
+    credentials['APP_SECRECT'],
+    credentials['OAUTH_TOKEN'],
+    credentials['OAUTH_TOKEN_SECRET'])
 
 def getHotLink():
-	for submission in reddit.subreddit('Ethereum').hot(limit=10):
-	    print(submission.url)
-	    # return(submission.url)
+  imageExtensions = ['.gif','.jpeg','.jpg','.jif','.jfif','.jp2','.jpx','.j2k','.j2c','.fpx','.pcd','.png','.pdf','www.reddit.com']
+
+  for submission in reddit.subreddit('Ethereum').hot():
+      if not any(extensions in submission.url for extensions in imageExtensions):
+        return(submission.url)
+
+def postToTwitter(submissionUrl):
+  twitter = getTwitter()
+  tweet = submissionUrl + " #Ethereum #ETH"
+  twitter.update_status(status=tweet)
+  print(tweet)
 
 try:
-	getHotLink()
+  postToTwitter(getHotLink())
 except Exception as e:
-	print(e)
+  print(e)

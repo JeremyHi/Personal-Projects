55771af23ef5b29ba3590b687313bb8488b4021f
Jeremy <jhitchco@lion.lmu.edu>
2017-08-10 11:12:17 -0700

    Created new validation for urls so no reposts

--- Ethereum_Twitter/EthereumLinks.py
+++ Ethereum_Twitter/EthereumLinks.py
@@ -1,43 +1,45 @@
 #!/usr/bin/env python
 
-import praw
+import praw, json, requests
 import RedditCredentials as rc
 import TwitterCredentials as tc
 from twython import Twython
 
-credentials = rc.getTwitterCredentials()
-reddit = praw.Reddit(
-  client_id=credentials['CLIENT_ID'],
-  client_secret=credentials['CLIENT_SECRECT'],
-  user_agent=credentials['USER_AGENT'])
+def getReddit():
+  redditCredentials = rc.getRedditCredentials()
+  return praw.Reddit(
+  client_id=redditCredentials['CLIENT_ID'],
+  client_secret=redditCredentials['CLIENT_SECRECT'],
+  user_agent=redditCredentials['USER_AGENT'])
 
 def getTwitter():
-  credentials = tc.getTwitterCredentials()
+  twitterCredentials = tc.getTwitterCredentials()
   return Twython(
-    credentials['APP_KEY'],
-    credentials['APP_SECRECT'],
-    credentials['OAUTH_TOKEN'],
-    credentials['OAUTH_TOKEN_SECRET'])
+    twitterCredentials['APP_KEY'],
+    twitterCredentials['APP_SECRECT'],
+    twitterCredentials['OAUTH_TOKEN'],
+    twitterCredentials['OAUTH_TOKEN_SECRET'])
+
+def checkIfRecentTweet(url):
+  twitter = getTwitter()
+  previousTweets = twitter.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=Current_ETH&count=40")
+  previousUrls = [tweet['entities']['urls'][0]['expanded_url'] for tweet in previousTweets if tweet['entities']['urls']]
+
+  return url in previousUrls
 
 def getHotLink():
+  reddit = getReddit()
   imageExtensions = ['.gif','.jpeg','.jpg','.jif','.jfif','.jp2','.jpx','.j2k','.j2c','.fpx','.pcd','.png','.pdf','www.reddit.com']
+  linkList = [submission.url for submission in reddit.subreddit('Ethereum').hot(limit=20) if not any(extensions in submission.url for extensions in imageExtensions) and checkIfRecentTweet(submission.url)]
 
-  linkList = []
-  for submission in reddit.subreddit('Ethereum').hot(limit=20):
-      if not any(extensions in submission.url for extensions in imageExtensions):
-        linkList.append(submission.url)
-  
   return linkList
 
 def postToTwitter(urlList):
   twitter = getTwitter()
   for url in urlList:
-    try:
-      tweet = url + " #Ethereum #ETH"
-      print(tweet)
-      return twitter.update_status(status=tweet)
-    except Exception as e:    
-      print(e)
+    tweet = url + " #Ethereum #ETH"
+    print(tweet)
+    return twitter.update_status(status=tweet)
 
 try:
   postToTwitter(getHotLink())
